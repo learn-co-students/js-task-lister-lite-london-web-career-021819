a77e6e1a51915773c0fbed2eeabcbe1b1bf4d70a
Alex Bujenita <dekc89@gmail.com>
2019-04-02 21:21:25 +0100

    sort done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+let ascOrder = true;
 const elForm = document.getElementById('create-task-form');
 const elUl = document.getElementById('tasks');
-
+const inputs = {
+  high: [],
+  medium: [],
+  low: []
+};
 // Creating LI
 
 const addLi = (userInput) => {
@@ -10,9 +15,29 @@ const addLi = (userInput) => {
   const priority = priorityEl.options[priorityEl.selectedIndex].value;
   elLi.className = `todo-el ${priority}`;
   elLi.textContent = userInput;
+  
+  inputs[priority].push(elLi);
+  
   elUl.appendChild(elLi);
 }
 
+// Create sorted Array
+
+function sortPriority() {
+  const elLi = document.createElement('li');
+  elLi.setAttribute('id', `${Date.now()+Math.floor(Math.random()*10000)}`);
+  if (ascOrder) {
+    inputs.high.forEach(elem => elUl.appendChild(elem))
+    inputs.medium.forEach(elem => elUl.appendChild(elem))
+    inputs.low.forEach(elem => elUl.appendChild(elem))
+    ascOrder = false;
+  } else {
+    ascOrder = true;
+    inputs.low.forEach(elem => elUl.appendChild(elem))
+    inputs.medium.forEach(elem => elUl.appendChild(elem))
+    inputs.high.forEach(elem => elUl.appendChild(elem))
+  }
+}
 
 
 // Add listeners
