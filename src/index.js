6823ca008462b6b8965768da42ad00ef80146944
mariamjaludi <mariam.jaludi@gmail.com>
2019-04-02 14:48:05 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+
+const formEl = document.querySelector('#create-task-form')
+const listEl = document.querySelector('#tasks')
+function addNewTask(text) {
+  //create li
+  const taskEl = document.createElement('li')
+  //add text to li
+  taskEl.innerText = text;
+  //find the List
+  //appwns the child to the parent.
+  listEl.appendChild(taskEl)
+}
+
+document.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const text = formEl['new-task-description'].value
+  addNewTask(text);
 });
