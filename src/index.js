e6cfc6d4fcc7c6c640cfdf6297726c32ac6feb38
schazbot <charlotte.pearce1984@gmail.com>
2019-10-17 12:01:38 +0100

    adding a backend, jsonserver up and running

--- src/index.js
+++ src/index.js
@@ -1,46 +1,77 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const formEl = document.querySelector('#create-task-form')
-  const listEl = document.querySelector('#tasks')
 
-  // add a single task
-  function addTask(text, name) {
-    debugger
-    // 	create LI
-    const todoEl = document.createElement('div')
-    //  create button
-    const btn = document.createElement("button");
-    btn.innerHTML = "X";
-    btn.className = "delete"
-    
-    //  add the text to the div
-    const taskText = document.createElement('li')
-    taskText.innerText = `${text}: ${name}`
-    todoEl.className = 'todo'
+// API stuff
 
-    //  append text, name and button to the LI
-    taskText.append(btn)
-    todoEl.append(taskText)
-    //  append LI to list
-    listEl.appendChild(todoEl)
-
-  }
-
-  // listen to form submission
-
-  formEl.addEventListener('submit', (event) => {
-    debugger
-    event.preventDefault()
-    const text = formEl.task.value
-    const name = formEl.person.value
-    addTask(text, name)
-    formEl.reset()
-  })
-
-  document.addEventListener('click', (event) => {
-    if (event.target.className === 'delete') {
-      event.target.parentElement.remove()
+function get(url) {
+  return fetch(url).then(resp => resp.json())
+}
+function post(url, id, data) { 
+  return fetch(`${url}${id}`,
+    {
+      method: 'POST',
+      headers: apiHeaders,
+      body: JSON.stringify(data)
     }
+  )
+}
 
-  })
+function destroy(url, id) {
+  return fetch(`${url}${id}`).then(resp => resp.json())
+}
 
-});
+const API = { get, destroy }
+const apiHeaders
+ = {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+}
+const choresUrl = "http://localhost:3000/chores/"
+const formEl = document.querySelector('#create-task-form')
+const listEl = document.querySelector('#tasks')
+
+
+///get all the saved tasks
+function getAllTasks() {
+  API.get(choresUrl).then(tasks => tasks.forEach(task => appendTask(task)))
+}
+
+
+// add a single task
+function appendTask(task) {
+  // 	create LI
+  const todoEl = document.createElement('div')
+  //  create button
+  const btn = document.createElement("button");
+  btn.innerHTML = "X";
+  btn.className = "delete"
+
+  //  add the text to the div
+  const taskText = document.createElement('li')
+  taskText.innerText = `${task.text}: ${task.name}`
+  todoEl.className = 'todo'
+
+  //  append text, name and button to the LI
+  taskText.append(btn)
+  todoEl.append(taskText)
+  //  append LI to list
+  listEl.appendChild(todoEl)
+}
+
+// listen to form submission
+
+formEl.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const taskObj = {
+    text: formEl.task.value,
+    name: formEl.person.value
+  }
+  API.post()
+  appendTask(taskObj)
+  formEl.reset()
+})
+
+document.addEventListener('click', (event) => {
+  if (event.target.className === 'delete') {
+    event.target.parentElement.remove()
+  }
+})
+
